875a6c32fa1803548cb06730f802141c978426dc
Anton Jacobsson <ajacobsson98@gmail.com>
2020-01-24 23:49:48 +0100

    style(navbar): styling

--- src/navbar.js
+++ src/navbar.js
@@ -10,19 +10,36 @@ class Navbar extends React.Component {
 
 render() {
   return (
-    <header>
-    <nav className="navbar navbar-dark bg-dark">
-    <Link to="/articles">Anton Jacobsson</Link>
-    <ul className="nav">
-    <li className="nav-item">
-    <Link to="/articles">Articles</Link>
-</li>
-<li className="nav-item">
-<Link to="/about">About</Link>
-</li>
-</ul>
-    </nav>
-  </header>
+//     <header>
+//     <nav className="navbar navbar-dark bg-dark">
+//     <Link to="/articles">Anton Jacobsson</Link>
+//     <ul className="nav">
+//     <li className="nav-item">
+//     
+// </li>
+// <li className="nav-item">
+// <Link to="/about">About</Link>
+// </li>
+// </ul>
+//     </nav>
+//   </header>
+<header>
+  <nav className="navbar navbar-expand-sm custom-navbar-primary">
+    <a className="custom-navbar-brand" href="#">Navbar</a>
+    <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+
+      <li className="nav-item">
+      <Link className={"nav-link"} to="/articles">Articles</Link>
+      </li>
+      <li className="nav-item">
+      <Link className={"nav-link"} to="/about">About</Link>
+      </li>
+      <li className="nav-item">
+      <Link className={"nav-link"} to="/github">Github</Link>
+      </li>
+    </ul>
+  </nav>
+</header>
    )
  }
 }
