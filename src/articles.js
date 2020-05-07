09e6225202041d58a0966133ec29508d6446bf71
Anton Jacobsson <ajacobsson98@gmail.com>
2020-05-07 21:03:34 +0200

    feat(articles): allow user to filter on must used tags

--- src/articles.js
+++ src/articles.js
@@ -12,7 +12,8 @@ class Articles extends React.Component {
     super(props);
     this.state = {
       articles: [],
-      filters: []
+      filters: [],
+      tags: []
     }
   }
 
@@ -47,11 +48,40 @@ class Articles extends React.Component {
       posts.reverse()
       this.setState({
         articles: posts,
-      })
+      });
+
+      const tags = await strapi.getEntries('tags');
+      this.toTagEntities(tags);
+
     }
     catch (err) {
       alert(err);
     }
+  }
+
+  toTagEntities(tags) {
+    var list = [];
+
+    tags.forEach(element => {
+      let dto =
+      {
+        name: element.name,
+        weight: element.articles.length
+      }
+      list.push(dto);
+    });
+
+    this.setState({
+      tags: list,
+    });
+  }
+
+  calculateFontSize(weight) {
+    const baseValue = 0.6;
+    const multiplier = 0.3;
+
+    const fontSize = (baseValue + (multiplier * Number(weight)));
+    return fontSize.toString() + "em";
   }
 
   render() {
@@ -60,67 +90,81 @@ class Articles extends React.Component {
       <div>
         <Helmet>
           <title>AJacobsson - Articles</title>
-          <meta name="description" content="Here you can find articles about software development" />
+          <meta name="description" content="Here you can find Articles about Web development with images and code examples." />
         </Helmet>
 
         <section>
           <div className="jumbotron jumbotron-fluid">
             <div className="container">
               <h1 className="display-4">Articles</h1>
-              <p className="lead">Here you can find articles about software development</p>
+              <p className="lead">Here you can find Articles about Web development with images and code examples.</p>
             </div>
           </div>
 
           <div className="container">
-
-            <div className="row" style={{marginBottom: '0px'}}>
-              <div className="card-body fixed-height-card">
-                {this.state.filters.map(tag => {
-                  return (
-                    <button key={tag} type="button" className="btn btn-outline-secondary custom-button-badge" onClick={() => this.removeFilterOnClick(tag)} >{tag}<span className="button-close-symbol">&#10006;</span></button>
-                  )
-                })}
+            <div className="row" style={{ maxWidth: 'inherit' }}>
+              <div className="col-lg-3">
+                <h6>Commonly used tags:</h6>
+                <div className="tags-box">
+                  {this.state.tags.map(tag => {
+                    return (
+                      <button key={tag.name} style={{ fontSize: this.calculateFontSize(tag.weight) }} type="button" className="btn" onClick={() => this.addFilterOnClick(tag.name)}>{tag.name}</button>
+                    )
+                  })}
+                </div>
               </div>
-            </div>
+              <div className="col-lg-8">
 
-            {this.state.articles.map(i => {
-              if (this.state.filters.length === 0 || i.tags.some(r => this.state.filters.includes(r.name)) === true) {
-                return (
-                  <div className="row" key={i.id}>
-                    <div>
-                      <div className="card-body">
+                <div className="fixed-height-card">
+                  <h6>Selected filters:</h6>
+                  {this.state.filters.map(tag => {
+                    return (
+                      <button key={tag} type="button" className="btn btn-outline-secondary custom-button-badge" onClick={() => this.removeFilterOnClick(tag)} >{tag}<span className="button-close-symbol">&#10006;</span></button>
+                    )
+                  })}
+                </div>
 
-                        <Link to={`/articles/${i.id}/${i.link}`}><h5 className="articles-title">{i.title}</h5></Link>
+                {this.state.articles.map(i => {
+                  if (this.state.filters.length === 0 || i.tags.some(r => this.state.filters.includes(r.name)) === true) {
+                    return (
+                      <div className="row" key={i.id}>
+                        <div>
+                          <div className="card-body">
 
-                        <div className="article-middle">
-                          <span className="articles-span">{i.user.username.substring(0, 1)}</span>
-                          <div>
-                            <span>Author</span>
-                            <span>{i.user.username}</span>
+                            <Link to={`/articles/${i.id}/${i.link}`}><h5 className="articles-title">{i.title}</h5></Link>
+
+                            <div className="article-middle">
+                              <span className="articles-span">{i.user.username.substring(0, 1)}</span>
+                              <div>
+                                <span>Author</span>
+                                <span>{i.user.username}</span>
+                              </div>
+                              <div>
+                                <span>Published</span>
+                                <span>{i.created_at.substring(0, 10)}</span>
+                              </div>
+                            </div>
+
+                            <p className="card-text">{i.description}</p>
+                            <div className="articles-badge-row">
+                              {i.tags.map((tag, index) => {
+                                return (
+                                  <button key={index} type="button" className={`btn btn-outline-secondary custom-button-badge ${this.state.filters.includes(tag.name) && 'active'}`} onClick={() => this.addFilterOnClick(tag.name)} >{tag.name}</button>
+                                )
+                              })}
+                              <Link to={`/articles/${i.id}/${i.link}`}><span>Continue reading..</span></Link>
+                            </div>
                           </div>
-                          <div>
-                            <span>Published</span>
-                            <span>{i.created_at.substring(0, 10)}</span>
-                          </div>
-                        </div>
-
-                        <p className="card-text">{i.description}</p>
-                        <div className="articles-badge-row">
-                          {i.tags.map((tag, index) => {
-                            return (
-                              <button key={index} type="button" className={`btn btn-outline-secondary custom-button-badge ${this.state.filters.includes(tag.name) && 'active'}`} onClick={() => this.addFilterOnClick(tag.name)} >{tag.name}</button>
-                            )
-                          })}
-                          <Link to={`/articles/${i.id}/${i.link}`}><span>Continue reading..</span></Link>
                         </div>
                       </div>
-                    </div>
-                  </div>
-                )
-              } else {
-                return <span key={i.id}></span>;
-              }
-            })}
+                    )
+                  } else {
+                    return <span key={i.id}></span>;
+                  }
+                })}
+              </div>
+              <div className="col-lg-1"></div>
+            </div>
           </div>
         </section>
       </div>
